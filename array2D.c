940b618e9b791a1dacbc9603691d0b0361d6413f
ranj2022 <ranjanaraj2019@gmail.com>
2022-03-19 07:32:31 +0000

    Its 2-D array C program to read / write

---
+++ array2D.c
@@ -0,0 +1,25 @@
+// reading and writing 2-D arrays
+#include<stdio.h>
+
+int main(){
+
+	int matrix[3][3];
+	int i,j;
+
+	printf("Enter elements into 3*3 matrix");
+
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			scanf("%d",&matrix[i][j]);
+		}
+	}
+
+    for(i=0;i<3;i++){
+    	printf("\n");
+		for(j=0;j<3;j++){
+			printf("%d ",matrix[i][j]);
+	}
+    printf("\n");
+}
+}
+
